d6a6192213a8dbed998ba8869fbd39df888ed42a
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-02-15 18:33:56 +1030

    first init

--- Item.h
+++ Item.h
@@ -1,53 +1,29 @@
-#ifndef ITEM_LIST_H
-#define ITEM_LIST_H
-#include "Item.h"
+#ifndef ITEM_H
+#define ITEM_H
 #include<string>
 #include<iostream>
-#include<string>
 using namespace std;
 
-class Potion: public Item
+class Item
 {
-    protected:
-    //State
-    int type;
-    double effect_list[2] = {1, 1};
-    string name_list[3] = {"ATTACK", "DEFENSE", "PREMIUM"};
+protected:
+	//Basic state of item
+	int grade;
+	int price;
+	string name;
 
-    public:
-    //Constructor that take in grade and type
-    Potion(int potion_grade, int potion_type);
+public:
+	//Behaviour
+	//Use item and apply its effect
+	virtual double* use_item()=0;
 
-    //Behaviour
-    //Use item and apply its effect
-    virtual double* use_item();
+	// Return index
+	virtual int return_index()=0;
 
-    //Return index
-    virtual int return_index();
-
-    //Destructor
-    ~Potion();
+	//Method that work with class state
+	int get_grade();
+	int get_price();
+	string get_name();
 };
 
-class Monball: public Item
-{
-    protected:
-    //State
-    double probability_list[3] = {80, 50, 20};
-    string name_list[3] = {"NORMAL", "GREAT", "MASTER"};
-
-    public:
-    //Constructor that take in grade
-    Monball(int ball_grade);
-
-    //Behaviour
-    //Use item and apply its effect
-    virtual double* use_item();
-
-    //Return index
-    virtual int return_index();
-
-    //Destructor
-    ~Monball();
-};
 #endif
